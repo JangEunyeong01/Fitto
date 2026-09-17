bf6333177bffbcf5bcfa2bdcbe06bae310bb71f3
Eunyeong Jang <jey4258@gmail.com>
2026-09-18 01:55:42 +0900

    설정 오류 원인이 먼저 보이도록 검사 시점 변경

--- server/src/main/java/com/fitto/server/ServerApplication.java
+++ server/src/main/java/com/fitto/server/ServerApplication.java
@@ -3,11 +3,16 @@ package com.fitto.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fitto.server.config.StartupSecurityCheck;
+
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication application = new SpringApplication(ServerApplication.class);
+		// 설정을 읽은 직후 위험한 값을 검사한다. 빈이 만들어지기 전이라 원인이 로그 맨 앞에 남는다.
+		StartupSecurityCheck.register(application);
+		application.run(args);
 	}
 
 }
